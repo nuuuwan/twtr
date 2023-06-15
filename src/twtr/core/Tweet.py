c1779d2e7349cdc9b2afc2e1c573a74092a306b9
nuuuwan <nuuuwan@gmail.com>
2023-06-15 13:03:39 +0530

    Added add_images

--- src/twtr/core/Tweet.py
+++ src/twtr/core/Tweet.py
@@ -3,6 +3,10 @@ class Tweet:
         self.text = text
         self.image_path_list = []
 
+    def add_images(self, image_path_list: list[str]):
+        self.image_path_list.extend(image_path_list)
+        return self
+
     def add_image(self, image_path: str):
-        self.image_path_list.append(image_path)
+        self.add_images([image_path])
         return self
--- tests/test_twitter_basic.py
+++ tests/test_twitter_basic.py
@@ -3,7 +3,9 @@ import unittest
 
 from twtr import Tweet, Twitter
 
+TEST_TWEET_TEXT = "Hello World!"
 TEST_IMG_PATH = os.path.join("tests", "test.png")
+TEST_IMG_PATH2 = os.path.join("tests", "test2.png")
 TEST_TWITTER = Twitter()
 SKIP_REASON_TWITTER = "Calls Twitter API"
 
@@ -15,12 +17,12 @@ class Test(unittest.TestCase):
 
     @unittest.skip(SKIP_REASON_TWITTER)
     def test_send_text_only(self):
-        tweet_id = TEST_TWITTER.send(Tweet("Hello World!"))
+        tweet_id = TEST_TWITTER.send(Tweet(TEST_TWEET_TEXT))
         self.assertIsNotNone(tweet_id)
 
     @unittest.skip(SKIP_REASON_TWITTER)
     def test_send_text_and_image(self):
         tweet_id = TEST_TWITTER.send(
-            Tweet("Hello World!").add_image(TEST_IMG_PATH)
+            Tweet(TEST_TWEET_TEXT).add_images([TEST_IMG_PATH, TEST_IMG_PATH2])
         )
         self.assertIsNotNone(tweet_id)
