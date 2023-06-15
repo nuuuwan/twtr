e5bdc8f07fad1b13aeb0d89140aa2c3414fedce4
nuuuwan <nuuuwan@gmail.com>
2023-06-15 17:19:28 +0530

    Revert "renamed to script_test.sh"
    This reverts commit f2d0aa15e6953c8ef95857ba12af9f7d3e307533.

--- tests/test_twitter_basic.py
+++ tests/test_twitter_basic.py
@@ -6,20 +6,23 @@ from twtr import Tweet, Twitter
 TEST_TWEET_TEXT = "Hello World!"
 TEST_IMG_PATH = os.path.join("tests", "test.png")
 TEST_IMG_PATH2 = os.path.join("tests", "test2.png")
+TEST_TWITTER = Twitter()
 SKIP_REASON_TWITTER = "Calls Twitter API"
 
 
 class Test(unittest.TestCase):
     @unittest.skip(SKIP_REASON_TWITTER)
+    def test_init_(self):
+        self.assertIsNotNone(TEST_TWITTER)
+
+    @unittest.skip(SKIP_REASON_TWITTER)
     def test_send_text_only(self):
-        twitter = Twitter()
-        tweet_id = twitter.send(Tweet(TEST_TWEET_TEXT))
+        tweet_id = TEST_TWITTER.send(Tweet(TEST_TWEET_TEXT))
         self.assertIsNotNone(tweet_id)
 
     @unittest.skip(SKIP_REASON_TWITTER)
     def test_send_text_and_image(self):
-        twitter = Twitter()
-        tweet_id = twitter.send(
+        tweet_id = TEST_TWITTER.send(
             Tweet(TEST_TWEET_TEXT).add_images([TEST_IMG_PATH, TEST_IMG_PATH2])
         )
         self.assertIsNotNone(tweet_id)
