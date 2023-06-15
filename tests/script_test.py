e71485fddd19e2c9bed02654ed5bb51d6dbdcade
nuuuwan <nuuuwan@gmail.com>
2023-06-15 17:00:30 +0530

    added time_id

--- tests/script_test.py
+++ tests/script_test.py
@@ -1,10 +1,12 @@
+from utils import get_time_id
 
 from twtr import Tweet, Twitter
 
 
 def main():
+    time_id = get_time_id()
     twitter = Twitter()
-    tweet = Tweet('Hello World from Script!')
+    tweet = Tweet(f'Hello World from {time_id}')
     twitter.send(tweet)
 
 
